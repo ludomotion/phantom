839240f0ddbaccefc74d8b02041b8474093eab3b
Koen Bollen <meneer@koenbollen.nl>
2012-11-26 21:23:28 +0100

    Added message delay components.

---
+++ Misc/Components/DelayedMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phantom.Core;
+
+namespace Phantom.Misc.Components
+{
+    public class DelayedMessage : Component
+    {
+        private float delay;
+        private int message;
+        private object data;
+
+        public DelayedMessage(float delay, int message, object data)
+        {
+            this.delay = delay;
+            this.message = message;
+            this.data = data;
+        }
+
+        public override void Update(float elapsed)
+        {
+            if (this.delay > 0 && (this.delay -= elapsed) <= 0)
+            {
+                this.delay = 0;
+                this.Parent.HandleMessage(this.message, this.data);
+                this.Destroyed = true;
+            }
+            base.Update(elapsed);
+        }
+    }
+}
---
+++ Misc/Components/MessageDelay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phantom.Core;
+using System.Diagnostics;
+
+namespace Phantom.Misc.Components
+{
+    public class MessageDelay : Component
+    {
+        private float delay;
+        private int[] messages;
+
+        private List<Tuple<float, int, object>> queue;
+
+        public MessageDelay(float delay, params int[] messages)
+        {
+            this.delay = delay;
+            if( messages.Length > 0 )
+                this.messages = messages;
+            this.queue = new List<Tuple<float, int, object>>();
+        }
+
+        public override Component.MessageResult HandleMessage(int message, object data)
+        {
+            if (this.messages == null || this.delayMessage(message))
+            {
+                this.queue.Add(Tuple.Create<float, int, object>(PhantomGame.Game.TotalTime, message, data));
+                return MessageResult.HANDLED;
+            }
+            return base.HandleMessage(message, data);
+        }
+
+        public override void Update(float elapsed)
+        {
+            float now = PhantomGame.Game.TotalTime;
+            for (int i = 0; i < this.queue.Count; ++i)
+            {
+                Tuple<float, int, object> r = this.queue[i];
+                if (now - r.Item1 > this.delay)
+                {
+                    this.HandleMessage(r.Item2, r.Item3);
+                    continue;
+                }
+                break;
+            }
+            base.Update(elapsed);
+        }
+
+        private bool delayMessage(int message)
+        {
+            if (message == Messages.SetPosition)
+                Debugger.Break();
+            for (int i = 0; i < this.messages.Length; ++i)
+                if (this.messages[i] == message)
+                    return true;
+            return false;
+        }
+    }
+}
