20e1327d046d00546d0d1715c49e6d765429de8b
Joris Dormans <jd@jorisdormans.nl>
2012-12-02 16:21:22 +0100

    Added sinoid lerping for camera

---
+++ Cameras/Components/SinoidLerp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Phantom.Cameras.Components
+{
+    public class SinoidLerp : CameraComponent
+    {
+        private float duration;
+        private float timer;
+        private Vector2 start;
+        private Vector2 target;
+        private bool lerping;
+        private bool started;
+
+        public SinoidLerp(float duration)
+        {
+            this.duration = duration;
+            this.lerping = false;
+            this.started = false;
+        }
+
+        public override void Update(float elapsed)
+        {
+            if (this.Camera.Target != this.Camera.Position && !started)
+            {
+                this.start = this.Camera.Position;
+                this.target = this.Camera.Target;
+                this.lerping = true;
+                this.timer = 0;
+                this.started = true;
+            }
+            if (this.lerping)
+            {
+                this.timer += elapsed;
+                float l = MathHelper.Clamp(this.timer / this.duration, 0, 1);
+                l = (float)(0.5 - 0.5 * Math.Cos(l * Math.PI));
+                this.Camera.Target = Vector2.Lerp(this.start, this.target, l);
+                if (this.timer >= this.duration)
+                {
+                    this.lerping = false;
+                    this.Camera.Target = this.Camera.Position = this.target;
+                }
+            }
+            base.Update(elapsed);
+        }
+    }
+}
---
+++ Cameras/Components/SinoidZoom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Phantom.Cameras.Components
+{
+    public class SinoidZoom : CameraComponent
+    {
+        private float startzoom;
+        private float targetzoom;
+
+        private float duration;
+        private float timer;
+
+        public SinoidZoom(float zoom, float duration)
+        {
+            this.targetzoom = zoom;
+            this.duration = duration;
+        }
+
+        public override void OnAdd(Core.Component parent)
+        {
+            base.OnAdd(parent);
+            this.startzoom = this.Camera.Zoom;
+            this.timer = 0;
+        }
+
+        public override void Update(float elapsed)
+        {
+            this.timer += elapsed;
+            float l = MathHelper.Clamp(this.timer / this.duration, 0, 1);
+            l = (float)(0.5 - 0.5 * Math.Cos(l * Math.PI));
+            this.Camera.Zoom = MathHelper.Lerp(this.startzoom, this.targetzoom, l);
+            if (this.timer >= this.duration)
+                this.Destroyed = true;
+            base.Update(elapsed);
+        }
+    }
+}
