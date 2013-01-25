3cd0e2ae3ee93e8e2719c0ca91eae6e97d69a30b
Koen Bollen <meneer@koenbollen.nl>
2013-01-26 00:50:41 +0100

    Added CameraOffset component.

---
+++ Cameras/Components/CameraOffset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Phantom.Cameras.Components
+{
+	public class CameraOffset : CameraComponent
+	{
+		private Vector2 offset;
+
+		public CameraOffset(Vector2 offset)
+		{
+			this.offset = offset;
+		}
+
+		public override void Update(float elapsed)
+		{
+			base.Update(elapsed);
+			if (this.Camera != null )
+			{
+				this.Camera.Target += this.offset;
+			}
+		}
+	}
+}
