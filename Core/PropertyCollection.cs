52b3239714cd50016c06091694c7188b34b569ae
Joris Dormans <jd@jorisdormans.nl>
2013-02-05 13:56:54 +0100

    More functionality in Properties

--- Core/PropertyCollection.cs
+++ Core/PropertyCollection.cs
@@ -17,5 +17,49 @@ namespace Phantom.Core
                 return ints;
             }
         }
+
+        private Dictionary<string, float> floats;
+        public Dictionary<string, float> Floats
+        {
+            get
+            {
+                if (floats == null)
+                    floats = new Dictionary<string, float>();
+                return floats;
+            }
+        }
+
+        private Dictionary<string, Object> objects;
+        public Dictionary<string, Object> Objects
+        {
+            get
+            {
+                if (objects == null)
+                    objects = new Dictionary<string, Object>();
+                return objects;
+            }
+        }
+
+        public int GetInt(string name, int defaultValue)
+        {
+            if (ints.ContainsKey(name))
+                return ints[name];
+            return defaultValue;
+        }
+
+        public float GetFloat(string name, float defaultValue)
+        {
+            if (floats.ContainsKey(name))
+                return floats[name];
+            return defaultValue;
+        }
+
+        public object GetObject(string name, object defaultValue)
+        {
+            if (objects.ContainsKey(name))
+                return objects[name];
+            return defaultValue;
+        }
+        
     }
 }
