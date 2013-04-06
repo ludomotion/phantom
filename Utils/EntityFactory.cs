3145acb970a36c0fdb972a7bf07ed9bb8c35cdd8
Joris Dormans <jd@jorisdormans.nl>
2013-04-06 10:43:27 +0200

    First pass of the PCN notation
    Note JSON did not suit as it doesn't support enough parameter types

---
+++ Utils/EntityFactory.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phantom.Core;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Phantom.Misc;
+using System.Diagnostics;
+using Phantom.Utils;
+
+namespace PhantomUtils
+{
+    /// <summary>
+    /// The EntityFactory can create entities with components from a blueprint specifying the
+    /// entity and component classes. You have to add the references of each assembly in the 
+    /// project seperatly first. Call the AddTypes method for each assembly first. Typically
+    /// you'll want to add the following lines to your main game class:
+    /// 
+    /// EntityFactory.AddTypes(Assembly.GetAssembly(typeof(Game1)));
+    /// EntityFactory.AddTypes(Assembly.GetAssembly(typeof(PhantomGame)));
+    /// 
+    /// </summary>
+    public static class EntityFactory
+    {
+        private static Dictionary<string, Type> components;
+
+        /// <summary>
+        /// This function adds a reference to all components and entities of an assembly to
+        /// the factories list. It does not overwrite reference to components and entities
+        /// with the same name. In that case it generates a warning instead.
+        /// To prevent a component or entity class from being added to the factory put the 
+        /// following static property in the class definition:
+        /// 
+        /// public static bool InFactory { get { return false; } }
+        /// 
+        /// </summary>
+        /// <param name="assembly"></param>
+        static public void AddTypes(Assembly assembly)
+        {
+            if (components == null)
+                components = new Dictionary<string, Type>();
+
+            Type[] types = assembly.GetTypes();
+            for (int i = 0; i < types.Length; i++)
+            {
+                PropertyInfo pi = types[i].GetProperty("InFactory", BindingFlags.FlattenHierarchy | BindingFlags.Static | BindingFlags.Public);
+                bool inFactory = !(pi != null && !(bool)pi.GetValue(null, null));
+
+                if (types[i].IsSubclassOf(typeof(Component)) && inFactory)
+                {
+                    //Trace.WriteLine(types[i].Name+" is a Component");
+                    if (components.ContainsKey(types[i].Name))
+                        Trace.WriteLine("WARNING: Could not add component '" + types[i].Name + "' because a component with the same name already exists.");
+                    else
+                        components[types[i].Name] = types[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Assembles an entity following the specifications in the blueprint
+        /// </summary>
+        /// <param name="blueprint">A string describing the entity in PhantomComponentNotion format</param>
+        /// <returns></returns>
+        static public Entity AssembleEntity(PCNComponent blueprint)
+        {
+            return (Entity)AssembleComponent(blueprint);
+        }
+
+        /// <summary>
+        /// Assembles a component  following the specifications in the blueprint
+        /// </summary>
+        /// <param name="blueprint">A string describing the component in PhantomComponentNotion format</param>
+        /// <returns></returns>
+        public static Component AssembleComponent(PCNComponent blueprint)
+        {
+            if (!components.ContainsKey(blueprint.Name))
+                throw new Exception("Trying to create unknown Component of type '" + blueprint.Name + "'");
+
+            Type componentType = components[blueprint.Name];
+            List<Type> types = new List<Type>();
+            List<Object> parameters = new List<object>();
+            AddParameters(blueprint, types, parameters);
+
+            Component component;
+            if (types.Count > 0)
+            {
+                ConstructorInfo cinfo = componentType.GetConstructor(types.ToArray());
+                component = (Component)cinfo.Invoke(parameters.ToArray());
+            }
+            else
+            {
+                component = (Component)Activator.CreateInstance(componentType);
+            }
+
+            for (int i = 0; i < blueprint.Components.Count; i++)
+                component.AddComponent(AssembleComponent(blueprint.Components[i]));
+
+            return component;
+        }
+
+        private static void AddParameters(PCNComponent component, List<Type> types, List<object> parameters)
+        {
+            for (int i = 0; i < component.Members.Count; i++)
+            {
+                if (component.Members[i].Value != null)
+                {
+                    types.Add(component.Members[i].Value.GetType());
+                    parameters.Add(component.Members[i].Value);
+                }
+            }
+        }
+    }
+}
---
+++ Utils/PhantomComponentNotation.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Phantom.Misc;
+using Phantom.Core;
+
+namespace Phantom.Utils
+{
+    public class PCNMember
+    {
+        public string Name;
+        public object Value;
+
+        public PCNMember(string description)
+        {
+            //Trace.WriteLine("Parse member: " + description);
+            int p = description.IndexOf('=');
+            if (p >= 0)
+            {
+                Name = description.Substring(0, p).Trim();
+                Value = PhantomComponentNotation.StringToValue(description.Substring(p + 1).Trim());
+            }
+            else
+            {
+                Name = description.Trim();
+                Value = true;
+                return;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + "=" + PhantomComponentNotation.ValueToString(Value);
+        }
+    }
+
+    public class PCNComponent
+    {
+        public string Name;
+        public List<PCNComponent> Components;
+        public List<PCNMember> Members;
+
+        public PCNComponent(string description)
+        {
+            Members = new List<PCNMember>();
+            Components = new List<PCNComponent>();
+            description = description.Trim();
+            int depthB = 0;
+            int depthP = 0;
+            int doubleQuotes = 0;
+            int singleQuotes = 0;
+            Name = null;
+            int memberStart = 0;
+            int componentStart = 0;
+
+            for (int i = 0; i < description.Length; i++)
+            {
+                if (description[i] == '"' && singleQuotes == 0)
+                    doubleQuotes = 1 - doubleQuotes;
+                if (description[i] == '\'' && doubleQuotes == 0)
+                    singleQuotes = 1 - singleQuotes;
+                if (singleQuotes == 0 && doubleQuotes == 0)
+                {
+                    if (description[i] == '(' && depthB == 0)
+                    {
+                        if (depthP == 0 && Name == null && i > 0)
+                            Name = description.Substring(0, i);
+                        depthP++;
+                        if (depthP == 1)
+                            memberStart = i + 1;
+                    }
+                    if (description[i] == '[' && depthP == 0)
+                    {
+                        if (depthB == 0 && Name == null && i > 0)
+                            Name = description.Substring(0, i);
+                        depthB++;
+                        if (depthB == 1)
+                            componentStart = i + 1;
+                    }
+                    if (description[i] == ')' && depthP > 0)
+                    {
+                        depthP--;
+                        if (depthP == 0)
+                        {
+                            string member = description.Substring(memberStart, i - memberStart).Trim();
+                            if (member != "")
+                                Members.Add(new PCNMember(member));
+                        }
+                    }
+                    if (description[i] == ',' && depthP == 1)
+                    {
+                        string member = description.Substring(memberStart, i - memberStart).Trim();
+                        if (member != "")
+                            Members.Add(new PCNMember(member));
+                        memberStart = i + 1;
+                    }
+                    if (description[i] == ']' && depthB > 0)
+                    {
+                        depthB--;
+                        if (depthB == 0)
+                        {
+                            string component = description.Substring(componentStart, i - componentStart).Trim();
+                            if (component != "")
+                                Components.Add(new PCNComponent(component));
+                        }
+                    }
+                    if (description[i] == ' ' && depthB == 1)
+                    {
+                        string component = description.Substring(componentStart, i - componentStart).Trim();
+                        if (component != "")
+                            Components.Add(new PCNComponent(component));
+                        componentStart = i + 1;
+                    }
+                }
+            }
+
+            if (Name == null)
+                Name = description;
+            //Trace.WriteLine("Component name: " + name);
+        }
+
+        public override string ToString()
+        {
+            string result = Name;
+            if (Members.Count > 0)
+            {
+                result += "(";
+                for (int i = 0; i < Members.Count; i++)
+                {
+                    if (i > 0) result += ",";
+                    result += Members[i].ToString();
+                }
+                result += ")";
+            }
+            if (Components.Count > 0)
+            {
+                result += "[";
+                for (int i = 0; i < Components.Count; i++)
+                {
+                    if (i > 0) result += " ";
+                    result += Components[i].ToString();
+                }
+                result += "]";
+            }
+            return result;
+        }
+    }
+
+    static class PhantomComponentNotation
+    {
+        public static string ComponentToPCNString(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object StringToValue(string v)
+        {
+            //null
+            if (v == "" || v == "null")
+                return null;
+            //boolean: false
+            if (v == "false")
+                return false;
+            //boolean: true
+            if (v == "true")
+                return true;
+            //string: 'XXX'
+            if (v[0] == '\'' && v[v.Length - 1] == '\'')
+                return v.Substring(1, v.Length - 2);
+            //string: "XXX"
+            if (v[0] == '"' && v[v.Length - 1] == '"')
+                return v.Substring(1, v.Length - 2);
+            //float: 0.0f
+            if (v[v.Length - 1] == 'f')
+            {
+                float f = 0;
+                float.TryParse(v.Substring(0, v.Length - 1), out f);
+                return f;
+            }
+            //color: #000000
+            if (v.StartsWith("#"))
+            {
+                int c = 0;
+                int.TryParse(v.Substring(1), System.Globalization.NumberStyles.HexNumber, null, out c);
+                return c.ToColor();
+            }
+            //hex: 0x0
+            if (v.StartsWith("0x"))
+            {
+                int c = 0;
+                int.TryParse(v.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out c);
+                return c;
+            }
+            //vector: (0,0) | (0,0,0) | (0,0,0,0)
+            if (v[0] == '(' && v[v.Length - 1] == ')')
+            {
+                v = v.Substring(1, v.Length - 2);
+                string[] m = v.Split(',');
+                int l = m.Length;
+                float[] f = new float[l];
+                for (int i = 0; i < l; i++)
+                {
+                    f[i] = 0;
+                    float.TryParse(m[i].Trim(), out f[i]);
+                }
+                if (l == 1 || l > 4)
+                    return null;
+                else if (l == 2)
+                    return new Vector2(f[0], f[1]);
+                else if (l == 3)
+                    return new Vector3(f[0], f[1], f[2]);
+                else if (l == 4)
+                    return new Vector4(f[0], f[1], f[2], f[3]);
+            }
+
+            int j = 0;
+            int.TryParse(v, out j);
+            return j;
+            
+        }
+
+        public static string ValueToString(object value)
+        {
+            if (value is string)
+                return "\"" + value + "\"";
+            if (value is bool)
+                return ((bool)value).ToString();
+            if (value is int)
+                return ((int)value).ToString();
+            if (value is float)
+                return ((float)value).ToString() + "f";
+            if (value is Color)
+                return "#" + ((Color)value).R.ToString("X2") + ((Color)value).G.ToString("X2") + ((Color)value).B.ToString("X2");
+            if (value is Vector2)
+                return "(" + ((Vector2)value).X.ToString() + "," + ((Vector2)value).Y.ToString() + ")";
+            if (value is Vector3)
+                return "(" + ((Vector3)value).X.ToString() + "," + ((Vector3)value).Y.ToString() + "," + ((Vector3)value).Z.ToString() + ")";
+            if (value is Vector4)
+                return "(" + ((Vector4)value).X.ToString() + "," + ((Vector4)value).Y.ToString() + "," + ((Vector4)value).Z.ToString() + "," + ((Vector4)value).W.ToString() + ")";
+            return "null";
+        }
+
+    }
+}
