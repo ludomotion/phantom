ae4fa3632ca0c5d0172c7a9e016b6087f98d6552
Joris Dormans <jd@jorisdormans.nl>
2013-04-11 10:56:32 +0200

    Added documentation to Phantom.Core.Component

--- Core/Component.cs
+++ Core/Component.cs
@@ -8,6 +8,12 @@ using System.Diagnostics;
 
 namespace Phantom.Core
 {
+    /// <summary>
+    /// The Component class is the heart of the Phantom engine. It follows a lean implementation
+    /// of the composite pattern where all components are also composites. Almost all main classes
+    /// in Phantom are derived from Component (inlcuding Entity, Layer, and GameState), which
+    /// means that all these classes can be nested as components.
+    /// </summary>
     public class Component : IDisposable
     {
         public enum MessageResult
@@ -17,11 +23,39 @@ namespace Phantom.Core
             CONSUMED
         }
 
+        /// <summary>
+        /// A uint that can be used to store arbitrary values. It has no fixed purpose but can
+        /// be used to represent type information. For example an Entity with flag 1 is the player,
+        /// while 2 idicates enemies, and so on.
+        /// </summary>
         public uint Flags { get; set; }
+
+        /// <summary>
+        /// Set the Destroyed flag to true to signal the parent of this component to remove this
+        /// component at the next opportunity.
+        /// </summary>
         public bool Destroyed { get; set; }
+
+        /// <summary>
+        /// The Ghost flag marks components that are not active, but are not destroyed either. 
+        /// Ghost components will not be updated and are not intergrated if they are part of 
+        /// and physics integrator.
+        /// </summary>
         public bool Ghost;
+
+        /// <summary>
+        /// The PropertyCollection of a component contains an arbitrary set of values stored in
+        /// dictionaries. Properties can be used to pass information between components without
+        /// building direct references. For normal Components the PropertyCollection is null. Only
+        /// Entities will always instanteate a collection in its constructor.
+        /// </summary>
         public PropertyCollection Properties;
 
+        /// <summary>
+        /// A collection of Components that are nested within this Component. Use AddComponent to
+        /// add components to this list, and RemoveComponent to remove them from the list. You
+        /// can also remove Components by setting their Destroyed flag to true.
+        /// </summary>
         public IList<Component> Components
         {
             get
@@ -30,10 +64,16 @@ namespace Phantom.Core
             }
         }
 
+        /// <summary>
+        /// The Component's parent indicates in which Component this Component is set.
+        /// </summary>
         public Component Parent { get; private set; }
 
         private List<Component> components;
 
+        /// <summary>
+        /// The default constructor generates a new Components and sets its flags to 0.
+        /// </summary>
         public Component()
         {
             this.Flags = 0;
@@ -41,71 +81,129 @@ namespace Phantom.Core
             this.components = new List<Component>();
         }
 
+        /// <summary>
+        /// Clear the component from memory and clean up any references
+        /// </summary>
         public virtual void Dispose()
         {
+            //TODO: Clean Parent and Components, Dispose children?
         }
 
+        /// <summary>
+        /// Function called after this Component is added to a parent Component.
+        /// </summary>
+        /// <param name="parent"></param>
         public virtual void OnAdd( Component parent )
         {
             this.Parent = parent;
             this.OnAncestryChanged();
         }
 
+        /// <summary>
+        /// Function called in response to any changes in its ancestors; when any of its parent or parent's parents calls OnAdd
+        /// </summary>
         public virtual void OnAncestryChanged()
         {
             for (int i = 0; i < this.components.Count; i++)
                 this.components[i].OnAncestryChanged();
         }
 
+        /// <summary>
+        /// Function called when this Component is removed from its parent.
+        /// </summary>
         public virtual void OnRemove()
         {
             this.Parent = null;
         }
 
-        protected virtual void OnComponentAdded(Component component)
+        /// <summary>
+        /// Function called when a child component is added to this component
+        /// </summary>
+        /// <param name="child"></param>
+        protected virtual void OnComponentAdded(Component child)
         {
-            component.OnAdd(this);
+            child.OnAdd(this);
         }
 
-        protected virtual void OnComponentRemoved(Component component)
+        /// <summary>
+        /// Function called when a child is removed from this component.
+        /// </summary>
+        /// <param name="child"></param>
+        protected virtual void OnComponentRemoved(Component child)
         {
-            component.OnRemove();
+            child.OnRemove();
         }
 
-        public void AddComponent(Component component)
+        /// <summary>
+        /// Call this function to add a child component to this component. This function
+        /// calls the OnAdd of the child, the OnAncestryChanged of the child and all its
+        /// children, and the OnComponentAdded of this component.
+        /// </summary>
+        /// <param name="child"></param>
+        public void AddComponent(Component child)
         {
-            this.components.Add(component);
-            this.OnComponentAdded(component);
-        }
-        
-        public void InsertComponent(int index, Component component)
-        {
-            this.components.Insert(index, component);
-            this.OnComponentAdded(component);
+            this.components.Add(child);
+            this.OnComponentAdded(child);
         }
 
-        public void InsertBeforeComponent(Component other, Component component)
+        /// <summary>
+        /// Call this function to insert a child component to this component at a specific location. 
+        /// This function calls the OnAdd of the child, the OnAncestryChanged of the child and all its
+        /// children, and the OnComponentAdded of this component.
+        /// </summary>
+        /// <param name="index">Zero based index, when the index is 0 the child is inserted as the first of this components children</param>
+        /// <param name="child"></param>
+        public void InsertComponent(int index, Component child)
+        {
+            this.components.Insert(index, child);
+            this.OnComponentAdded(child);
+        }
+
+        /// <summary>
+        /// Call this function to insert a child component to this component before a specific component. 
+        /// This function calls the OnAdd of the child, the OnAncestryChanged of the child and all its
+        /// children, and the OnComponentAdded of this component.
+        /// </summary>
+        /// <param name="other">A existing child of the component, the new child is added before this component</param>
+        /// <param name="child"></param>
+        public void InsertBeforeComponent(Component other, Component child)
         {
             // TODO: Test if the index is correct or if it needs a -1.
-            this.components.Insert(this.components.IndexOf(other), component);
-            this.OnComponentAdded(component);
+            this.components.Insert(this.components.IndexOf(other), child);
+            this.OnComponentAdded(child);
         }
 
-        public void RemoveComponent(Component component)
+        /// <summary>
+        /// Call this function to remove a component a child from this component. This function calls
+        /// the OnRemove of the child and the OnComponentRemoved of this component.
+        /// </summary>
+        /// <param name="child"></param>
+        public void RemoveComponent(Component child)
         {
-            this.components.Remove(component);
-            this.OnComponentRemoved(component);
-
+            this.components.Remove(child);
+            child.OnRemove();
+            this.OnComponentRemoved(child);
         }
 
+        /// <summary>
+        /// Removes all children from this component, starting with the last child.
+        /// </summary>
         public virtual void ClearComponents()
         {
             for (int i = this.components.Count - 1; i >= 0; i--)
-            {
                 this.RemoveComponent(this.components[i]);
-            }
         }
 
+        /// <summary>
+        /// HandleMessage is the prefered way of communication between components. HandleMessage
+        /// processes the message and passes it to its children unless it returns MessageResult.CONSUMED 
+        /// at any point.
+        /// You are adviced to be careful with passing messages high up to component tree as by default 
+        /// all components pass messages to all their children.
+        /// </summary>
+        /// <param name="message">Int representing the message. The Phantom Messages class contains standard messages, you are advised to create a list for messages for your projects</param>
+        /// <param name="data"></param>
+        /// <returns>Returns a MessageResult indicating the message was Ignored (no component handled it), Handled (at least one component handled it), or Consumed (one component handled it and prevented the message from being passed further along).</returns>
         public virtual MessageResult HandleMessage( int message, object data )
         {
             MessageResult finalResult = MessageResult.IGNORED;
@@ -120,6 +218,10 @@ namespace Phantom.Core
             return finalResult;
         }
 
+        /// <summary>
+        /// The generic Update method is called once every frame. Override this method to implement behavior that needs to be updated every frame.
+        /// </summary>
+        /// <param name="elapsed">The time between the current frame and the previous frame, measured in seconds.</param>
         public virtual void Update(float elapsed)
         {
             for (int i = this.components.Count - 1; i >= 0; i--)
@@ -134,6 +236,11 @@ namespace Phantom.Core
             }
         }
 
+        /// <summary>
+        /// The generic Integrate method that is called by a physics integrator a specified number of times per frame. Override this method
+        /// to implement physics behavior.
+        /// </summary>
+        /// <param name="elapsed">The simulated time between the this integration call and the previous call, measured in seconds.</param>
         public virtual void Integrate(float elapsed)
         {
             for (int i = this.components.Count - 1; i >= 0; i--)
@@ -145,6 +252,12 @@ namespace Phantom.Core
             }
         }
 
+        /// <summary>
+        /// Check to see if the entity this component is part of can physically collide with another entity. Return true by default. 
+        /// If any of the components contained by each Entity returns false, the entities cannot collide.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>If any of the components contained by each Entity returns false, the entities cannot collide.</returns>
         public virtual bool CanCollideWith(Entity other)
         {
             for (int i = 0; i < this.components.Count; i++ )
@@ -153,6 +266,11 @@ namespace Phantom.Core
             return true;
         }
 
+        /// <summary>
+        /// Override this method to respond to collisions between this component's entity and another entity.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <param name="collision"></param>
         public virtual void AfterCollisionWith(Entity other, CollisionData collision)
         {
             for (int i = 0; i < this.components.Count; i++)
@@ -160,6 +278,11 @@ namespace Phantom.Core
                 
         }
 
+        /// <summary>
+        /// A components and its children are rendered by this method. Render is called when a component is part 
+        /// of a RenderLayer or an EntityLayer.
+        /// </summary>
+        /// <param name="info"></param>
         public virtual void Render( RenderInfo info )
         {
             int count = this.components.Count;
@@ -236,6 +359,11 @@ namespace Phantom.Core
         
         //*/
 
+        /// <summary>
+        /// Get a list of all the component's children that match a certain type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public IEnumerable<T> GetAllComponentsByType<T>() where T : Component
         {
             int count = this.Components.Count;
@@ -261,6 +389,12 @@ namespace Phantom.Core
             return result;
         }
         
+        /// <summary>
+        /// Find and return a child component by its type. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nth">A zero based index of the match to return (0 returns the first child of type T, 1 the second, and so on).</param>
+        /// <returns></returns>
         public T GetComponentByType<T>(int nth) where T : Component
         {
             int count = this.Components.Count;
@@ -278,11 +412,21 @@ namespace Phantom.Core
             return null;
         }
 
+        /// <summary>
+        /// Find and return the first child component of type T. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public T GetComponentByType<T>() where T : Component
         {
             return this.GetComponentByType<T>(0);
         }
 
+        /// <summary>
+        /// Find and return the first ancestor of type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public T GetAncestor<T>() where T : Component
         {
             Component iter = this.Parent;
