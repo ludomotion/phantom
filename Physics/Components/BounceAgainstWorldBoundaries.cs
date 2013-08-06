29808574543c8a406c861f7494fc4c29dbcfe586
Joris Dormans <jd@jorisdormans.nl>
2013-08-06 18:52:26 +0200

    Improvements to bounceAgainstWorldBoundaries

--- Physics/Components/BounceAgainstWorldBoundaries.cs
+++ Physics/Components/BounceAgainstWorldBoundaries.cs
@@ -10,11 +10,26 @@ namespace Phantom.Physics.Components
     {
         private float restitution;
         private float threshold;
+        private float width;
+        private float height;
 
         public BounceAgainstWorldBoundaries(float threshold, float restitution)
         {
             this.restitution = -restitution;
             this.threshold = threshold;
+            width = PhantomGame.Game.Width;
+            height = PhantomGame.Game.Height;
+        }
+
+        public override void OnAncestryChanged()
+        {
+            base.OnAncestryChanged();
+            EntityLayer entities = GetAncestor<EntityLayer>();
+            if (entities != null)
+            {
+                width = entities.Bounds.X;
+                height = entities.Bounds.Y;
+            }
         }
 
         public override void Integrate(float elapsed)
@@ -31,15 +46,15 @@ namespace Phantom.Physics.Components
                 if (this.Entity.Mover.Velocity.Y < 0)
                     this.Entity.Mover.Velocity.Y *= this.restitution;
             }
-            if (this.Entity.Position.X + this.threshold > PhantomGame.Game.Width)
+            if (this.Entity.Position.X + this.threshold > width)
             {
-                this.Entity.Position.X = PhantomGame.Game.Width - this.threshold;
+                this.Entity.Position.X = width - this.threshold;
                 if (this.Entity.Mover.Velocity.X > 0)
                     this.Entity.Mover.Velocity.X *= this.restitution;
             }
-            if (this.Entity.Position.Y + this.threshold > PhantomGame.Game.Height)
+            if (this.Entity.Position.Y + this.threshold > height)
             {
-                this.Entity.Position.Y = PhantomGame.Game.Height - this.threshold;
+                this.Entity.Position.Y = height - this.threshold;
                 if (this.Entity.Mover.Velocity.Y > 0)
                     this.Entity.Mover.Velocity.Y *= this.restitution;
             }
