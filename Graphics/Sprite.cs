5f83540f0a5577d258a7664f5c86420c66490f54
Joris Dormans <jd@jorisdormans.nl>
2014-05-20 12:37:31 +0200

    Adds offset to sprites and optimizes rendering (stores source rectangles in stead of creating a new one for every render call)

--- Graphics/Sprite.cs
+++ Graphics/Sprite.cs
@@ -19,12 +19,15 @@ namespace Phantom.Graphics
 
         public bool Flipped { get; set; }
 
+        private readonly int offsetX;
+        private readonly int offsetY;
         public readonly int Width;
         public readonly int Height;
         public readonly float InverseWidth;
         public readonly float InverseHeight;
         public readonly Vector2 Size;
         public readonly Texture2D Texture;
+        private readonly Rectangle[] rects;
         public Vector2 Origin;
 
         private int horizontalFramesCount;
@@ -85,9 +88,12 @@ namespace Phantom.Graphics
         
 #endif
 
-        public Sprite(Texture2D texture, int width, int height, float centerX, float centerY)
+        public Sprite(Texture2D texture, int width, int height, float centerX, float centerY, int offsetX, int offsetY)
         {
             this.Flipped = false;
+
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
 
             this.Texture = texture;
             if (width <= 0 || height <= 0)
@@ -104,21 +110,25 @@ namespace Phantom.Graphics
             this.Size = new Vector2(width, height);
             this.Origin = new Vector2(centerX, centerY);
 
-            this.horizontalFramesCount = texture.Width / width;
-            this.verticalFramesCount = texture.Height / height;
+            this.horizontalFramesCount = (texture.Width - offsetX) / width;
+            this.verticalFramesCount = (texture.Height - offsetY) / height;
 
             this.FrameCount = this.horizontalFramesCount * this.verticalFramesCount;
+
+            this.rects = new Rectangle[this.FrameCount];
+            for (int i = 0; i < this.FrameCount; i++)
+                this.rects[i] = GetRectByFrame(i);
+
         }
+
+        public Sprite(Texture2D texture, int width, int height, float centerX, float centerY)
+            : this(texture, width, height, centerX, centerY, 0, 0) { }
 
         public Sprite(Texture2D texture, int width, int height)
-            :this(texture, width, height, width*0.5f, height*0.5f)
-        {
-        }
+            :this(texture, width, height, width*0.5f, height*0.5f, 0, 0) { }
 
         public Sprite(Texture2D tex)
-            :this(tex,0,0)
-        {
-        }
+            :this(tex,0,0) { }
 
         public void RenderFrame(RenderInfo info, int frame, Vector2 position, float angle, float scale, Color color, float alpha)
         {
@@ -137,7 +147,7 @@ namespace Phantom.Graphics
             color.R = (byte)(color.R * alpha);
             color.G = (byte)(color.G * alpha);
             color.B = (byte)(color.B * alpha);
-            Rectangle source = GetRectByFrame(frame);
+            Rectangle source = rects[Math.Max(Math.Min(frame, this.FrameCount-1), 0)];
             info.Batch.Draw(this.Texture, position, source, color, angle, Origin, scale, flipHorizontal ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
 
 #if DEBUG
@@ -152,8 +162,8 @@ namespace Phantom.Graphics
             color.R = (byte)(color.R * alpha);
             color.G = (byte)(color.G * alpha);
             color.B = (byte)(color.B * alpha);
-            Rectangle source = GetRectByFrame(frame);
-			Vector2 scale = new Vector2(drawSize.X / this.Width, drawSize.Y / this.Height);
+            Rectangle source = rects[Math.Max(Math.Min(frame, this.FrameCount - 1), 0)];
+            Vector2 scale = new Vector2(drawSize.X / this.Width, drawSize.Y / this.Height);
             info.Batch.Draw(this.Texture, position, source, color, angle, Origin, scale, flipHorizontal ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
 
 #if DEBUG
@@ -165,7 +175,7 @@ namespace Phantom.Graphics
         {
             if (frame < 0 || frame >= this.FrameCount)
                 return;
-            Rectangle source = GetRectByFrame(frame);
+            Rectangle source = rects[Math.Max(Math.Min(frame, this.FrameCount - 1), 0)];
             info.Batch.Draw(this.Texture, position, source, color, angle, Origin, scale, Flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
 #if DEBUG
             AddCall(Texture, scale);
@@ -176,8 +186,8 @@ namespace Phantom.Graphics
         {
             if (frame < 0 || frame >= this.FrameCount)
                 return;
-            Rectangle source = GetRectByFrame(frame);
-			Vector2 scale = new Vector2(drawSize.X / this.Width, drawSize.Y / this.Height);
+            Rectangle source = rects[Math.Max(Math.Min(frame, this.FrameCount - 1), 0)];
+            Vector2 scale = new Vector2(drawSize.X / this.Width, drawSize.Y / this.Height);
 			info.Batch.Draw(this.Texture, position, source, color, angle, Origin, scale, Flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
 #if DEBUG
 			AddCall(Texture, scale.X * 0.5f + scale.Y * 0.5f);
@@ -188,7 +198,7 @@ namespace Phantom.Graphics
         {
             if (frame < 0 || frame >= this.FrameCount)
                 return;
-            Rectangle source = GetRectByFrame(frame);
+            Rectangle source = rects[Math.Max(Math.Min(frame, this.FrameCount - 1), 0)];
             info.Batch.Draw(this.Texture, position, source, color, angle, Origin, scale, flipHorizontal ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
 #if DEBUG
             AddCall(Texture, scale);
@@ -219,7 +229,7 @@ namespace Phantom.Graphics
         {
             int x = frame % this.horizontalFramesCount;
             int y = frame / this.horizontalFramesCount;
-            return new Rectangle(x * this.Width, y * this.Height, this.Width, this.Height);
+            return new Rectangle(x * this.Width + offsetX, y * this.Height + offsetY, this.Width, this.Height);
         }
 
         /*public void DrawToSpriteBatch(SpriteBatch batch, int frame, Color color)
