196f43dc75de8fafe3f4f47041702a900fcfa145
Joris Dormans <jd@jorisdormans.nl>
2015-03-28 20:38:12 +0100

    Fixes issues in the editor

--- Phantom/GameUI/InputDialog.cs
+++ Phantom/GameUI/InputDialog.cs
@@ -17,13 +17,13 @@ namespace Phantom.GameUI
             : base(left, top, 400, 120, title)
         {
             this.onInput = onInput;
-            edit = new EditBox("edit", text, new Vector2(10+380*0.5f, 60*10), new OABB(new Vector2(380*0.5f, 20*0.5f)), 40);
+            edit = new EditBox(left+10, top+60, 380, 20, text, caption, EditBox.ValueType.String, null, null);
             edit.CaptionPosition = new Microsoft.Xna.Framework.Vector2(5, -30);
 
 
             AddComponent(edit);
-            AddComponent(new Button("bOK", "OK", new Vector2(200+40, 90+12),new OABB(new Vector2(40, 12)), Confirm));
-            AddComponent(new Button("bCancel", "Cancel", new Vector2(200+40, 90+12),new OABB(new Vector2(40, 12)), Cancel));
+            AddComponent(new Button("bOK", "OK", new Vector2(left+200+40, top+90+12),new OABB(new Vector2(40, 12)), Confirm));
+            AddComponent(new Button("bCancel", "Cancel", new Vector2(left + 300 + 40, top+90 + 12), new OABB(new Vector2(40, 12)), Cancel));
         }
 
         private void Confirm(UIElement sender, int player)
